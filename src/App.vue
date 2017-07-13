d4bba4aec7ac69ae56218d7e8ffcb15ef9985acd
fridolph <249121486@qq.com>
2017-07-13 19:54:32 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App'
+
+// import router from './router'
+
+import fastclick from 'fastclick'
+
+import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // router,
+  render: h => h(App)
+})
