c4355d0f55f3126ebba35fbd7bd2f1cd10f1564a
fridolph <249121486@qq.com>
2017-08-22 00:37:58 +0800

    公共方法及配置相关

--- src/api/config.js
+++ src/api/config.js
@@ -1,13 +1,13 @@
 /*
  * @Author: fridolph
  * @Date: 2017-07-17 23:08:57
- * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2017-07-17 23:15:16
+ * @Last Modified by: fridolph
+ * @Last Modified time: 2017-08-22 00:23:06
  */
 
 export const commonParams = {
   g_tk: 5381,
-  inCharset: 'utf-8',
+  inCharset: 'utf8',
   outChars: 'utf-8',
   notice: 0,
   format: 'jsonp'
---
+++ src/common/js/util.js
@@ -0,0 +1,5 @@
+export function log(name, data) {
+  console.log(`---------------------- ${name} ----------------------`)
+  console.log(data)
+  console.log('-----------------------------------------------------')
+}
