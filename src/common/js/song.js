a9c8f3fdc71a89ef8b31829adba16e54584f641c
fridolph <249121486@qq.com>
2017-08-22 23:52:00 +0800

    歌曲api~ 获取真实数据~ 拼接层 加上musicData.songid 后面的vkey照搬即可

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -2,7 +2,7 @@
  * @Author: fridolph 
  * @Date: 2017-08-22 00:39:51 
  * @Last Modified by: fridolph
- * @Last Modified time: 2017-08-22 00:56:20
+ * @Last Modified time: 2017-08-22 22:47:23
  */
 
 export default class Song {
@@ -36,7 +36,7 @@ export function createSong(musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `https://thirdparty.gtimg.com/${musicData.songid}.m4a?fromtag=38`,
+    url: `http://dl.stream.qqmusic.qq.com/${musicData.songid}.m4a?vkey=2F11A6BDAAAAAEC26B271FCD5B992C21E98F064CE6522A9B06AE05012C25A974D90431E6EFDB90D988A060CB4F70636C5B9A0C8D38E01626&guid=258171504&uin=0&fromtag=66`
   })
 }
 
