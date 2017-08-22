a5f53cc085cd548e8276cb79dc0e90acc116e383
fridolph <249121486@qq.com>
2017-08-22 22:22:01 +0800

    歌手详情~ 及歌曲页

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -11,8 +11,14 @@ import { mapGetters } from 'vuex'
 import {log} from 'common/js/util'
 import {getSingerDetail} from 'api/singer'
 import {ERR_OK} from 'api/config'
+import {createSong} from 'common/js/song'
 
 export default {
+  data() {
+    return {
+      song: []
+    }
+  },
   computed: {
     ...mapGetters([
       'singer'
@@ -28,9 +34,21 @@ export default {
       getSingerDetail(this.singer.id)
         .then(res => {
           if (res.code === ERR_OK) {
-            log('singer, 歌曲, list', res.data.list)
+            this.songs = this._normalizeSongs(res.data.list)
+            log('songs, 歌曲', this.songs)
           }
         })
+    },
+
+    _normalizeSongs(list) {
+      let ret = []
+      list.forEach(item => {
+        let {musicData} = item
+
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData))
+        }
+      })
     }
   },
 
